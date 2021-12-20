9ac9b98dd94814d7846d5be6e5030b8f5b7731dc
MayraMendoza <mayra601m@gmail.com>
2021-12-20 16:42:31 -0500

    Test commit

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -2,8 +2,49 @@ package com.careerdevs;
 
 public class Main {
 
+    //https://edabit.com/challenge/SquoES5RxWr4CyX6G
+    // 1. Create a function that takes an array containing only numbers and return the first element.
+    public static int getFirstValue(int[] arr) {
+        int firstNumber = arr[0];
+        return firstNumber;
+    }
+
+    //https://edabit.com/challenge/Qir2pQ2Qp3uwrfJNZ
+
+    //2. Create a function that takes a number as an argument, increments the number by +1 and returns the result
+    public static int addition(int num) {
+        int newNum = num + 1;
+        return newNum;
+    }
+    //https://edabit.com/challenge/3NowS55XTdfWtKh9n
+
+    // 3.Create a function that searches for the index of a given item in an array. If the item is present, it should return the index, otherwise, it should return -1.
+    public static int search(int arr[], int item) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == item)
+                return i;
+        }
+        return -1;
+    }
+
+    //https://edabit.com/challenge/2t8JDxF7wLrg7yJ5E
+    //4.Write a function that takes an integer minutes and converts it to seconds.
+    public static int convert(int minutes) {
+        int seconds = minutes * 60;
+        return seconds;
+
+    }
+
+
+    //https://edabit.com/challenge/ZuoKAgmH8G2vHtLzi
+    // Write a function that returns
+    public static String giveMeSomething(String a) {
+        a = "something" + " " + a;
+        return a;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello GitHub");
     }
 }
