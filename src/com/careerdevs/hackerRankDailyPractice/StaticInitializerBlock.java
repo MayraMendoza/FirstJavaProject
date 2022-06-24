c7c4884c9ba75cf16b254a60e8acc7a9f740c9f1
MayraMendoza <mayra601m@gmail.com>
2022-06-23 22:16:41 -0400

    10. Java Static Initializer Block

---
+++ src/com/careerdevs/hackerRankDailyPractice/StaticInitializerBlock.java
@@ -0,0 +1,12 @@
+package com.careerdevs.hackerRankDailyPractice;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+
+public class StaticInitializerBlock {
+
+}
